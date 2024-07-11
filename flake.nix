b8c47f73e51674afaad9611d59e3e6cea3878466
Juuso Elo-Rauta <juuso.er@gmail.com>
2024-07-11 19:19:19 +0300

    trying to patch awesome to a newer version, but to no avail...

--- flake.nix
+++ flake.nix
@@ -16,13 +16,33 @@
 
   outputs = inputs@{self, nixpkgs, nixpkgs-stable, ...}:
     let
+      # get a neewr version of awesomewm v4.3 is 4 years old...
+      awesome-git-overlay = final: prev : {
+        awesome = (prev.lib.removeAttrs 
+            (prev.awesome.overrideAttrs (old: {
+              version = "20240606-git";
+              src = prev.fetchFromGitHub {
+                owner = "awesomeWM";
+                repo = "awesome";
+                rev = "ad0290bc1aac3ec2391aa14784146a53ebf9d1f0";
+                sha256 = "uaskBbnX8NgxrprI4UbPfb5cRqdRsJZv0YXXshfsxFU=";
+              };
+            outputs = ["out"];
+            patches = [];
+          })) 
+          ["patches"]);
+        };
+
       pkgs = import nixpkgs {
         system = "x86_64-linux";
         config= {
           allowUnfree = true;
           allowUnfreePredicate = (_:true);
         };
-        overlays = [inputs.rust-overlay.overlays.default];
+        overlays = [
+          inputs.rust-overlay.overlays.default
+          awesome-git-overlay
+        ];
       };
       lib = nixpkgs.lib;
     in
